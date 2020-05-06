bd59947b9d3b880615ea9482227c17b5982d5ad8
Taskim <tony@evaneos.com>
2020-05-06 22:49:07 +0200

    fix(register): password length

--- app/src/Session/components/Register.tsx
+++ app/src/Session/components/Register.tsx
@@ -12,7 +12,7 @@ import { registerRequest } from '../actions'
 const SignupSchema = yup.object().shape({
     username: yup.string().min(4).required(),
     email: yup.string().email().required(),
-    password: yup.string().min(6).required(),
+    password: yup.string().min(8).required(),
 })
 
 type Props = {
