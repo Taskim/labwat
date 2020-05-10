57412f055d18a03337a74058202596fb2c9060ae
Taskim <tony@evaneos.com>
2020-05-10 17:45:37 +0200

    fix(safari): play a sound twice

--- backend/src/util/sound.ts
+++ backend/src/util/sound.ts
@@ -1,7 +1,11 @@
 import { v4 as uuidv4 } from 'uuid'
 
+// this function set the parameter dl to 1 and replace the dropbox url by dl.dropboxusercontent
+// in order to avoid bugs on safari: resetting the sound current time
 export const setDropboxDownloadParamater = (url: string): string =>
-    url.replace(/(dl=).*?(&|$)/g, '$1' + '1' + '$2')
+    url
+        .replace(/(dl=).*?(&|$)/g, '$1' + '1' + '$2')
+        .replace('www.dropbox', 'dl.dropboxusercontent')
 
 export const replaceFilenameByUuid = (filename: string): string =>
     filename.replace(/.*(?=\.)/g, uuidv4())
