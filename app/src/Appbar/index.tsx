c829802d3243a95dfc21e93c70daa0bf634a7470
Taskim <tony@evaneos.com>
2020-05-24 18:18:10 +0200

    feat(app): update logo and background

--- app/src/Appbar/index.tsx
+++ app/src/Appbar/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { AppBar as MuiAppBar, Toolbar, Typography } from '@material-ui/core'
+import { Link } from 'react-router-dom'
 
 import LoginOrProfile from './LoginOrProfile'
 import s from './style.module.css'
+import Logo from './Logo'
 
 function AppBar() {
     return (
@@ -12,9 +14,11 @@ function AppBar() {
             elevation={0}
         >
             <Toolbar variant="dense">
-                <Typography variant="h6" className={s.title}>
-                    Sonus
-                </Typography>
+                <div className={s.logo}>
+                    <Link to="/">
+                        <Logo color="#fff" />
+                    </Link>
+                </div>
                 <LoginOrProfile />
             </Toolbar>
         </MuiAppBar>
